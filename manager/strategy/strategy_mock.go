6dc27b0d0571cd3d472f59a4d7b609d0db9b8a89
Tom Hill <tomhill@notonthehighstreet.com>
2017-01-03 16:04:11 +0000

    Add simulation mode

---
+++ manager/strategy/strategy_mock.go
@@ -0,0 +1,22 @@
+package strategy
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/notonthehighstreet/autoscaler/manager/inventory"
+	"github.com/notonthehighstreet/autoscaler/manager/monitor"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockStrategy struct {
+	mock.Mock
+}
+
+func (m *MockStrategy) Evaluate() (*Recommendation, error) {
+	args := m.Mock.Called()
+	return args.Get(0).(*Recommendation), args.Error(1)
+}
+
+func MockNew(_ *viper.Viper, _ inventory.Inventory, _ monitor.Monitor, _ *logrus.Entry) Strategy {
+	return &MockStrategy{}
+}
