39ad263dd52b297b09b54988017daa19bce0b5b4
Tom Hill <tomhill@notonthehighstreet.com>
2017-02-15 14:21:46 +0000

    Move AWS inventory up into inventory package

--- manager/inventory/marathon_test.go
+++ manager/inventory/marathon_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/notonthehighstreet/autoscaler/manager/inventory"
 )
 
-var inv *inventory.Marathon
+var marathonInv *inventory.Marathon
 var mockClient inventory.MockMarathonClient
 
 func setupMarathonTest() {
@@ -23,8 +23,8 @@ func setupMarathonTest() {
 	config.Set("app", "notonthehighstreet-admin")
 	config.Set("url", "http://foo.com:8080")
 	i, _ := inventory.NewMarathon(config, log)
-	inv = i.(*inventory.Marathon)
-	inv.Client = &mockClient
+	marathonInv = i.(*inventory.Marathon)
+	marathonInv.Client = &mockClient
 	instances := 1
 	app := marathonclient.Application{Instances: &instances}
 	mockClient.On("ApplicationBy").Return(app, nil)
@@ -32,7 +32,7 @@ func setupMarathonTest() {
 
 func TestMarathon_Total(t *testing.T) {
 	setupMarathonTest()
-	total, _ := inv.Total()
+	total, _ := marathonInv.Total()
 	assert.Equal(t, total, 1)
 }
 
@@ -40,7 +40,7 @@ func TestMarathonInventory_Scale(t *testing.T) {
 	setupMarathonTest()
 	deployment := marathonclient.DeploymentID{}
 	mockClient.On("ScaleApplicationInstances").Return(deployment, nil)
-	err := inv.Scale(+1)
+	err := marathonInv.Scale(+1)
 	assert.NoError(t, err)
 }
 
@@ -48,36 +48,36 @@ func TestMarathon_Increase(t *testing.T) {
 	setupMarathonTest()
 	deployment := marathonclient.DeploymentID{}
 	mockClient.On("ScaleApplicationInstances").Return(deployment, nil)
-	assert.NoError(t, inv.Increase())
+	assert.NoError(t, marathonInv.Increase())
 
-	inv.Config.Set("maximum_instances", 1)
-	assert.Error(t, inv.Increase())
+	marathonInv.Config.Set("maximum_instances", 1)
+	assert.Error(t, marathonInv.Increase())
 }
 
 func TestMarathon_Decrease(t *testing.T) {
 	setupMarathonTest()
 	deployment := marathonclient.DeploymentID{}
 	mockClient.On("ScaleApplicationInstances").Return(deployment, nil)
-	assert.NoError(t, inv.Decrease())
+	assert.NoError(t, marathonInv.Decrease())
 
-	inv.Config.Set("minimum_instances", 1)
-	assert.Error(t, inv.Decrease())
+	marathonInv.Config.Set("minimum_instances", 1)
+	assert.Error(t, marathonInv.Decrease())
 }
 
 func TestMarathon_Status(t *testing.T) {
 	setupMarathonTest()
-	s, _ := inv.Status()
+	s, _ := marathonInv.Status()
 	assert.Equal(t, inventory.OK, s)
 }
 
-func TestSettleDownTime(t *testing.T) {
+func TestMarathon_SettleDownTime(t *testing.T) {
 	setupMarathonTest()
 	deployment := marathonclient.DeploymentID{}
 	mockClient.On("ScaleApplicationInstances").Return(deployment, nil)
-	inv.Config.Set("settle_down_period", "5m")
-	assert.Nil(t, inv.Increase())
-	s, _ := inv.Status()
+	marathonInv.Config.Set("settle_down_period", "5m")
+	assert.Nil(t, marathonInv.Increase())
+	s, _ := marathonInv.Status()
 	assert.Equal(t, inventory.UPDATING, s)
-	assert.Error(t, inv.Decrease())
-	assert.Error(t, inv.Increase())
+	assert.Error(t, marathonInv.Decrease())
+	assert.Error(t, marathonInv.Increase())
 }
